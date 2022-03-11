45eeb60ccd5d467e84f7adae5f023e9c89b5ccba
olderzeus <olderzeus@gmail.com>
2022-03-11 18:32:38 +0800

    Update parse_roms.py

--- parse_roms.py
+++ parse_roms.py
@@ -1072,7 +1072,7 @@ class ROMParser:
         total_img_size += img_size
         build_config += "#define ENABLE_EMULATOR_GW\n" if rom_size > 0 else ""
 
-        save_size, rom_size, img_size = self.generate_system(
+        save_size, rom_size, img_size, current_id = self.generate_system(
             "Core/Src/retro-go/msx_roms.c",
             "MSX",
             "msx_system",
@@ -1088,7 +1088,7 @@ class ROMParser:
         total_rom_size += rom_size
         total_img_size += img_size
         #bios
-        save_size, rom_size, img_size = self.generate_system(
+        save_size, rom_size, img_size, current_id = self.generate_system(
             "Core/Src/retro-go/msx_bios.c",
             "MSX_BIOS",
             "msx_bios",
